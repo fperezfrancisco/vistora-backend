57604719782525b4cab5bacefd9a92dcd63a8322
Francisco Perez <fperezfrancisco4@gmail.com>
2025-08-09 11:22:18 -0700

    fixed firebase admin page

--- middleware/verifyFirebaseToken.js
+++ middleware/verifyFirebaseToken.js
@@ -1,4 +1,4 @@
-const admin = require("../firebaseAdmin");
+const admin = require("../utils/firebaseAdmin");
 
 const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
